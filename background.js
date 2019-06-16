bf10101b419544119ee0b86fc984539d0ba5d854
Adeel Tariq <adeeltariqagain@gmail.com>
2019-06-16 18:15:03 +0500

    Only refresh if internet available

--- background.js
+++ background.js
@@ -7,7 +7,32 @@ try {
 
 myBrowser.runtime.onMessage.addListener(function(msg, sender, sendResponse) {
     if (msg.text == "Please refresh") {
-        chrome.tabs.reload(sender.tab.id);
+	ping("api.github.com", function(m){ if (m) chrome.tabs.reload(sender.tab.id); });
+        //chrome.tabs.reload(sender.tab.id);
         sendResponse({ text: "You got it" });
     }
 });
+
+/* Wait for a ping from github api to only tigger refresh when online */
+function ping(host, pong) {
+
+  var http = new XMLHttpRequest();
+
+  http.open("GET", "http://" + host, /*async*/true);
+  http.onreadystatechange = function() {
+
+    if (http.readyState == 4) {
+      
+	if (http.status == 200) {
+	      if (pong != null) {
+		pong(true);
+	      }
+	}
+    }
+  };
+  try {
+    http.send(null);
+  } catch(exception) {
+    // this is expected
+  }
+}
